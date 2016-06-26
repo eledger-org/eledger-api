1b28119ca2b86bfea2349b32d8000ae8790cdc16
Henry <hankbrown@gmail.com>
2016-06-26 14:33:46 -0700

    Added a really great test!

---
+++ test/index.js
@@ -0,0 +1,9 @@
+/* eslint-env mocha */
+
+var assert            = require("chai").assert;
+
+describe("This is a test!", function() {
+  it("should test things", function() {
+    assert.equal(1 + 1, 2);
+  });
+});
